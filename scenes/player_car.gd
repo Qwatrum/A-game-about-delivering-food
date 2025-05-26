b784fed4f2844c4445d23b457f9156721992ea1e
Qwatrum <171450552+Qwatrum@users.noreply.github.com>
2025-05-26 15:12:28 +0200

    Feature added
    You can now press space to spawn in the air if your are stuck

--- scenes/player_car.gd
+++ scenes/player_car.gd
@@ -3,4 +3,9 @@ extends VehicleBody3D
 
 func _physics_process(delta):
 	steering = Input.get_axis("right", "left") * 0.2
-	engine_force = Input.get_axis("back", "forward") * 120
+	engine_force = Input.get_axis("back", "forward") * 256
+	
+	
+	if Input.is_action_just_released("space"):
+		position.y += 0.5
+		rotation_degrees = Vector3(0,rotation_degrees.y,0)
